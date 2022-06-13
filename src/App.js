e9c463197ee2c9d3ef691f67bf9d0d046dbda70b
Karly-22 <karly.holloway@hotmail.com>
2022-06-13 11:50:49 +0100

    add Header and Nav bar components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import './App.css';
+import "./App.css";
+import { Routes, ROute } from "react-router-dom";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
 
 function App() {
-  return 
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Routes>
+        <Route path="/reviews" element={<ReviewsList />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Nav() {
+  return <nav>LInks</nav>;
+}
+
+export default Nav;
