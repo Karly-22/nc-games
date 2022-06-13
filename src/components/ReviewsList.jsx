2372f5625eef27bce4a5ddca2724e463f831e6eb
Karly-22 <karly.holloway@hotmail.com>
2022-06-13 12:52:32 +0100

    install axios and add fetchReviews function to get review data from API

--- src/components/ReviewsList.jsx
+++ src/components/ReviewsList.jsx
@@ -1,27 +1,23 @@
-import { useState, useEffect } from 'react';
-import ReviewCard from './ReviewCard';
+import { useState, useEffect } from "react";
+import { fetchReviews } from "../utils/api";
+import ReviewCard from "./ReviewCard";
 
 function ReviewsList() {
-    const [reviews, setReviews] = useState([{
-        "review_id": 14,
-        "owner": "cooljmessy",
-        "title": "Velit tempor ullamco amet ipsum dolor voluptate.",
-        "category": "hidden-roles",
-        "review_img_url": "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg",
-        "created_at": "2021-02-05T11:27:26.563Z",
-        "votes": 3,
-        "comment_count": 0
-      }]);
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+      fetchReviews().then((reviewsFromAPI) => {
+          setReviews(reviewsFromAPI);
+      })
+  }, []);
 
   return (
     <ul>
-        {reviews.map((review) => {
-            return (
-                <ReviewCard key={review.review_id} review={review} />
-            )
-        })}
+      {reviews.map((review) => {
+        return <ReviewCard key={review.review_id} review={review} />;
+      })}
     </ul>
-  )
+  );
 }
 
-export default ReviewsList
+export default ReviewsList;
