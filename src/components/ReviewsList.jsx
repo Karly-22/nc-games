d64b2abd99795b3e341ad85b606ae1b5276db7d3
Karly-22 <karly.holloway@hotmail.com>
2022-06-16 16:07:55 +0100

    add filters component to sort and order reviews

--- src/components/ReviewsList.jsx
+++ src/components/ReviewsList.jsx
@@ -1,32 +1,33 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { fetchReviews } from "../utils/api";
 import ReviewCard from "./ReviewCard";
-import CatDropdown from "./CatDropdown";
-import { useParams } from "react-router-dom"
+import Filters from "./Filters";
 
 function ReviewsList() {
   const [reviews, setReviews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  // const [userSelection, setUserSelection] = useState("");
+  const [sortBy, setSortBy] = useState("created_at");
+  const [orderBy, setOrderBy] = useState("DESC");
   const { category } = useParams();
-  
+
   useEffect(() => {
-    fetchReviews(category).then((reviewsFromAPI) => {
+    fetchReviews(category, sortBy, orderBy).then((reviewsFromAPI) => {
       setReviews(reviewsFromAPI);
       setIsLoading(false);
     });
-  }, [category]);
+  }, [category, sortBy, orderBy]);
 
   if (isLoading) return <div className="spinner"></div>;
   return (
-    <main>
-        {/* <CatDropdown setUserSelection={setUserSelection}/> */}
+    <section>
+      <Filters setSortBy={setSortBy} sortBy={sortBy} orderBy={orderBy} setOrderBy={setOrderBy}/>
       <ul className="review-list">
         {reviews.map((review) => {
           return <ReviewCard key={review.review_id} review={review} />;
         })}
       </ul>
-    </main>
+    </section>
   );
 }
 
