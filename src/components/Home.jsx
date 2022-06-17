775e6cabd165ae64c8c4efd7c7498b776f26bf82
Karly-22 <karly.holloway@hotmail.com>
2022-06-17 11:00:17 +0100

    add top 3 games to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { fetchReviews } from "../utils/api";
+import ReviewCard from "./ReviewCard";
+import { useContext } from "react";
+import { ThemeContext } from "../contexts/Theme";
 
 function Home() {
+  const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const theme = useContext(ThemeContext);
+
+  useEffect(() => {
+    fetchReviews("", "votes", "DESC").then((reviewsFromAPI) => {
+      setReviews(reviewsFromAPI);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) return <div className="spinner"></div>;
   return (
-    <div>HomePage</div>
-  )
+    <section>
+      <h3 className="homepage-header">Top 3 highest voted games:</h3>
+      <ul className={`review-list-${theme}`}>
+        {reviews.map((review) => {
+          return <ReviewCard key={review.review_id} review={review} />;
+        }).slice(0, 3)}
+      </ul>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
